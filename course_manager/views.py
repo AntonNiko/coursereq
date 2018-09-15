ddd6374d05bca4d898cea803695e3dd8cfdda101
Anton Nikitenko <ant.nikitenko@gmail.com>
2018-09-15 10:17:35 -0700

    Initial commit

---
+++ course_manager/views.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.db.models import Q
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.utils import timezone
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello")
